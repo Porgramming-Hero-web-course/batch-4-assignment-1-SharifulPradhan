519e0c5cee51677b01a791005364a045d71c8838
SharifulPradhan <sharifulpradhanhridoy@gmail.com>
2024-11-08 18:14:58 +0600

    problem 7 compiled

---
+++ dist/problem-7.js
@@ -0,0 +1,19 @@
+"use strict";
+{
+    class Car {
+        constructor(make, model, year) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        getCarAge() {
+            const currentYear = new Date().getFullYear();
+            return currentYear - this.year;
+        }
+    }
+    // Sample Input 1:
+    const car = new Car("Honda", "Civic", 2018);
+    car.getCarAge();
+    console.log(car.getCarAge());
+    // Sample Output 1: 6(assuming current year is 2024)
+}
